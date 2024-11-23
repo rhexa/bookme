c0a26ad8b92c7bfda1368d6ffe72c3125043726b
rhexa <akun.kripitit@gmail.com>
2024-11-23 23:05:28 +0200

    #8 init and configure frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import bookmeLogo from '/bookme.svg'
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to Bookme</h1>
+      <img src={bookmeLogo} alt="Bookme logo" />
+    </>
+  )
+}
+
+export default App
